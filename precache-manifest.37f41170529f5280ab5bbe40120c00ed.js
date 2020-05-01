1332350c1c0e6b3237d0cba1e65e1173de3b9112
ramonrpa <ramon.rpa2018@gmail.com>
2020-05-01 15:33:39 -0300

    Updates

---
+++ precache-manifest.37f41170529f5280ab5bbe40120c00ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0840ba53f1da51124d066a0af3eff2b",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "2eb101b4915b9b21199e",
+    "url": "/quiz-app/static/js/2.432a92fb.chunk.js"
+  },
+  {
+    "revision": "0ce5bbda0d44a33e41f4cf41e8bd7ab7",
+    "url": "/quiz-app/static/js/2.432a92fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e5a4a36ce9f979abe5",
+    "url": "/quiz-app/static/js/main.ae5f1c02.chunk.js"
+  },
+  {
+    "revision": "dd430ebde0356d1a798f",
+    "url": "/quiz-app/static/js/runtime-main.34bd227c.js"
+  }
+]);
